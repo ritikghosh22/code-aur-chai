f76e3992b2f40f9fe7410ce0448a6552da499c05
ritikghosh22 <113223965+ritikghosh22@users.noreply.github.com>
2024-04-22 13:24:58 +0000

    pritter

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId= 14435
+let accountEmail ="ritik@gmail.com"
+var accountPassword ="122434"
+accountCity = "bhopal"
+let accountState;
+
+// prefer not to use var
+//because of issue in block space adn functon scope
+
+
+accountEmail ="ritik@ghs"
+accountPassword="234122"
+accountcity="bengaluru"
+
+console.table([accountId,accountEmail,accountPassword,accountState,accountcity]);
---
+++ 02_datatypes.js
@@ -0,0 +1,18 @@
+let name ="ritik"
+let age=21
+let isLoggedIn = false
+
+let state =null
+
+// number => 2 to powe 53
+//bigint
+//string => =""
+//boolean => true/false
+//null => stadnalone value
+//undifened => 
+//symbol => unique
+
+//object
+
+console.log(typeof undifened); //undifened
+console.log(typeof null) //object
