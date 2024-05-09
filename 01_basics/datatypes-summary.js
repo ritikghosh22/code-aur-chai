30c0c41421afc42bca2151969a6beab690b2d3da
ritikghosh22 <113223965+ritikghosh22@users.noreply.github.com>
2024-05-09 11:13:57 +0000

    funtion

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,19 @@
+// primitive
+
+// 7 types: String , number, boolean, null ,undefined , symbol,Bigint
+
+
+// Reference(non primitive)
+
+//array,object , function
+
+const heros = ["saktiman","naagraj","doga"]
+
+let myObj = {
+    name:"ritik",
+    age:21,
+}
+
+const myFunction= function(){
+    console.loh("hellow world")
+}
