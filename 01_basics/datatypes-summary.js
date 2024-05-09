f298af405fef294dc7d75fc806266d4b0a866588
ritikghosh22 <113223965+ritikghosh22@users.noreply.github.com>
2024-05-09 12:32:47 +0000

    changes

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -1,19 +1,31 @@
-// primitive
+// // primitive
 
-// 7 types: String , number, boolean, null ,undefined , symbol,Bigint
+// // 7 types: String , number, boolean, null ,undefined , symbol,Bigint
 
 
-// Reference(non primitive)
+// // Reference(non primitive)
 
-//array,object , function
+// //array,object , function
 
-const heros = ["saktiman","naagraj","doga"]
+// const heros = ["saktiman","naagraj","doga"]
 
-let myObj = {
-    name:"ritik",
-    age:21,
-}
+// let myObj = {
+//     name:"ritik",
+//     age:21,
+// }
 
-const myFunction= function(){
-    console.loh("hellow world")
-}
+// const myFunction= function(){
+//     console.loh("hellow world")
+// }
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//  stack (primitive),heap(NoN - primitive)
+
+let myYoutube = " ritikghosh"
+
+let anothername= myYoutube
+anothername = "chaiaurcode"
+
+console.log(myYoutube)
+console.log(anothername)
